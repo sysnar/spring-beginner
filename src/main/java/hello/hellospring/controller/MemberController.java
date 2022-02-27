9286a4daaec4b345a5d0436d702cf3d33d8e4ece
sysnar <sysn4r@gmail.com>
2022-02-27 16:18:58 +0900

    feat: add spring component scan
    add controller @Autowired & @Controller
    add service @Autowired & @Service
    add repository @Repository

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,14 +1,16 @@
 package hello.hellospring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class MemberController {
 
-    @GetMapping("hello-spring")
-    @ResponseBody
-    public String helloString(@RequestParam("name") String name) {
-        return "hello" + name;
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
     }
 }
