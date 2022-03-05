3b7f666ca720aacc3d95a8d0913e6f41810c21eb
sysnar <sysn4r@gmail.com>
2022-03-05 12:58:53 +0900

    feat: app dependency injection with test

--- spring-core-1/src/main/java/hello/core/MemberApp.java
+++ spring-core-1/src/main/java/hello/core/MemberApp.java
@@ -1,9 +1,15 @@
-package hello.core.member;
+package hello.core;
+
+import hello.core.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
