15d36f1948e9dfcc091db67309e74843b912449c
sysnar <sysn4r@gmail.com>
2022-03-11 16:11:53 +0900

    feat: spring request scope and proxy

--- spring-core-1/src/main/java/hello/core/web/LogDemoController.java
+++ spring-core-1/src/main/java/hello/core/web/LogDemoController.java
@@ -15,13 +15,17 @@ import javax.servlet.http.HttpServletResponse;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
+
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
