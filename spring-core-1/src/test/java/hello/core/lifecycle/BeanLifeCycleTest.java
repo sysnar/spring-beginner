b5330060126963199c66632522f864eecdd04c7a
sysnar <sysn4r@gmail.com>
2022-03-11 12:25:52 +0900

    feat: bean cycle annotation
    @PostConstruct
    @PreDestroy

--- spring-core-1/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ spring-core-1/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -20,7 +20,8 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean(initMethod = "init", destroyMethod = "close")
+//        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
--- spring-core-1/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring-core-1/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient  {
 
     private String url;
@@ -30,12 +33,14 @@ public class NetworkClient  {
         System.out.println("close : " + url);
     }
 
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();
